f0feb77ade307593cac2320fe8931c5c19cbd587
Anis Mokadym <52089762+AnisMkd@users.noreply.github.com>
2019-06-23 17:35:23 +0200

    ajout de la géolocalisation

--- Météo.js
+++ Météo.js
@@ -1,13 +1,24 @@
+var latitude = "0";
+var longitude = "0";
+
+
 var callBackGetSuccess = function (data) {
     var Température = document.getElementById("zone_meteo_Temp");
     var Température_Temp_Max = document.getElementById("zone_meteo_Temp_Max");
     var Température_Temp_Min = document.getElementById("zone_meteo_Temp_Min");
-    var ValVille = document.getElementById("ville");
     Température.innerHTML = Math.round(data.main.temp) + "°C";
     Température_Temp_Max.innerHTML = Math.round(data.main.temp_max) + "°C";
     Température_Temp_Min.innerHTML = Math.round(data.main.temp_min) + "°C";
 }
 
+var callBackGetSuccessGeolocation = function (data) {
+    var Température = document.getElementById("zone_meteo_Temp");
+    var Température_Temp_Max = document.getElementById("zone_meteo_Temp_Max");
+    var Température_Temp_Min = document.getElementById("zone_meteo_Temp_Min");
+    Température.innerHTML = Math.round(data.main.temp) + "°C";
+    Température_Temp_Max.innerHTML = Math.round(data.main.temp_max) + "°C";
+    Température_Temp_Min.innerHTML = Math.round(data.main.temp_min) + "°C";
+}
 
 
 function BtnActualiseClickGET() {
@@ -28,18 +39,44 @@ function BtnActualiseClickGET() {
 function OuverturePage() {
     document.getElementById("ville").value = "Paris";
     BtnActualiseClickGET();
+    geolocalisation();
+    
 }
 
 
 function geolocalisation() {
+    geolocalisation2();
+    console.log(latitude);
+    console.log(longitude);
+    var url = ("https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=ea108f94d782f6ac139ea7e4d3525816&units=metric");
+
+    $.get(url, callBackGetSuccessGeolocation).done(function () {
+            //alert( "second success" );
+        })
+        .fail(function () {
+            document.getElementById("ville").value = "Recherche incorrect";
+        })
+        .always(function () {
+            //alert( "finished" );
+        });
+}
+
+function geolocalisation2() {
     if (navigator.geolocation)
         navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
     else
         alert("Votre navigateur ne prend pas en compte la géolocalisation HTML5");
 
+
+
     function successCallback(position) {
-        alert("Latitude : " + position.coords.latitude + ", longitude : " + position.coords.longitude);
+        latitude = position.coords.latitude;
+        longitude = position.coords.longitude;
+        rechercheville();
+
     };
+
+
 
     function errorCallback(error) {
         switch (error.code) {
@@ -54,4 +91,24 @@ function geolocalisation() {
                 break;
         }
     };
+}
+
+var callBackGetSuccessGeolocation2 = function (data) {
+    document.getElementById("ville").value = (data.address.town);
+    BtnActualiseClickGET();
+}
+
+function rechercheville() {
+    url = ("https://nominatim.openstreetmap.org/reverse?format=json&lat=" + latitude + "&lon=" + longitude + "&zoom=18&addressdetails=1");
+    $.get(url, callBackGetSuccessGeolocation2).done(function () {
+            //alert( "second success" );
+        })
+        .fail(function () {
+            document.getElementById("ville").value = "Recherche incorrect";
+        })
+        .always(function () {
+            //alert( "finished" );
+        });
+
+
 }
